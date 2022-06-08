008f17b2b3be1879091707a2df92b28bac3f27df
rajodiya-shubham <shubham.rajodiya.sr@gmail.com>
2022-06-09 00:58:22 +0530

    Added color changes and minor text change.

--- src/core/CommingSoon.tsx
+++ src/core/CommingSoon.tsx
@@ -5,8 +5,8 @@ type CommingSoonProps = {};
 const CommingSoon: React.FC<CommingSoonProps> = () => {
   return (
     <div className="mh-100">
-      <div className="banner d-flex align-items-center justify-content-center m-5 bg-light">
-        <h1>Comming Soon ....!</h1>
+      <div className="banner d-flex align-items-center justify-content-center">
+        <h1 style={{ color: "#FFA500" }}>Coming Soon...</h1>
       </div>
     </div>
   );
